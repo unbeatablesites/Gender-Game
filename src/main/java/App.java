1873fa0b9439515f17d2dbdbb6b4d2cb22ffcccb
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-18 09:11:05 -0500

    Create Logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        askGenderQuestion();
 
+        Scanner scan = new Scanner(System.in);
+
+        String genderAns = scan.nextLine();
+
+        askFirstNameQuestion();
     }
 
-    public static void askQuestion(){}
+    public static void askGenderQuestion(){
+        System.out.println("What is your gender (M or F):");
+    }
+
+    public static void askFirstNameQuestion(){
+        System.out.println("First name:");
+    }
+
+
 
 }
